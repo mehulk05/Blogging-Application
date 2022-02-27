eebf234f7bdb8bfa571d21bda20ea66bf81365d2
Mehul <mehul.kothari@growth99.com>
2022-02-27 10:25:16 +0530

    updated env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: '',
+  firebaseConfig : {
+    apiKey: "AIzaSyDysn47q8aR9VAuyaMLNk4VyqGnPXmLtEg",
+    authDomain: "upworkmedia-e4752.firebaseapp.com",
+    databaseURL: "https://upworkmedia-e4752-default-rtdb.firebaseio.com",
+    projectId: "upworkmedia-e4752",
+    storageBucket: "upworkmedia-e4752.appspot.com",
+    messagingSenderId: "196238148246",
+    appId: "1:196238148246:web:4da5c7648a48d3ee44f8b7"
+  }
 };
